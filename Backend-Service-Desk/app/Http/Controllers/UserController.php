d3d01b16cdaf7a3ca6809390edfc42598d4f9483
Alfathanf <alfathanfarrel0305@gmail.com>
2025-07-20 23:16:05 +0700

    farrel commit lupa

--- Backend-Service-Desk/app/Http/Controllers/UserController.php
+++ Backend-Service-Desk/app/Http/Controllers/UserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
-
+use Firebase\JWT\JWT;
 
 class UserController extends Controller
 {
