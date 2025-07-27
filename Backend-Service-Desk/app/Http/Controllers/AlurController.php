6443dcabc0f77e7975cd84305a431638e5a76a67
Damairf <damairaya33@gmail.com>
2025-07-27 21:31:04 +0700

    Raya pengelola post alur

--- Backend-Service-Desk/app/Http/Controllers/AlurController.php
+++ Backend-Service-Desk/app/Http/Controllers/AlurController.php
@@ -12,4 +12,14 @@ class AlurController extends Controller
         $jenis_pelayanan_alur = JenisPelayanan::with("alur_jnsPelayanan.isi_alur")->where('ID_Jenis_Pelayanan', $jnspelId)->get();
         return response()->json($jenis_pelayanan_alur);
     }
+    public function postAlur(Request $request){
+        $ID_Jenis_Pelayanan = $request->ID_Jenis_Pelayanan;
+        $ID_Isi_Alur = $request->ID_Isi_Alur;
+
+        $newAlurPelayanan = Alur::create([
+            'ID_Jenis_Pelayanan' => $ID_Jenis_Pelayanan,
+            'ID_Isi_Alur' => $ID_Isi_Alur,
+        ]);
+        return response(["message" => "Alur dari jenis layanan ditambahkan", "data" => $newAlurPelayanan]);
+    }
 }
