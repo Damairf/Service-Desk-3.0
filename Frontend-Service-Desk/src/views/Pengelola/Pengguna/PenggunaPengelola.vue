e1bcda3b6045b710df7aaf5ff8afb77462ab3cee
Alfathanf <alfathanfarrel0305@gmail.com>
2025-07-28 16:30:56 +0700

    farrelA commit nampilin backend user2

--- Frontend-Service-Desk/src/views/Pengelola/Pengguna/PenggunaPengelola.vue
+++ Frontend-Service-Desk/src/views/Pengelola/Pengguna/PenggunaPengelola.vue
@@ -14,7 +14,6 @@ onMounted(() => {
     }
   })
   .then(response => {
-    console.log(response.data)
     daftarPengguna.value = response.data.map(item => ({
       id: item.ID_User,
       nama_depan: item.Nama_Depan,
